f0cdbd4c38d31321ad8e846891d8bfa2b6c43cdd
Michael Dollosa <michaeldollosa@dolees-Macbook-Pro.local>
2021-08-25 06:42:51 +0800

    completed initial taks controller tests

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,11 +3,13 @@ class TasksController < ApplicationController
   #new is not created since we would render a form inside a different erb file
 
   def create
-
+    @task = Task.new(task_params)
+    @task.category_id = params[:category_id]
+    @task.save
   end
 
   def show
-
+    @task = Task.find(params[:id])
   end
   
   def edit
@@ -15,10 +17,18 @@ class TasksController < ApplicationController
   end
 
   def update
-
+    @task = Task.find(params[:id])
+    @task.update(task_params)
   end
 
   def destroy
+    @task = Task.find(params[:id])
+    @task.delete
+  end
 
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :body)
   end
 end
--- test/controllers/tasks_controller_test.rb
+++ test/controllers/tasks_controller_test.rb
@@ -1,6 +1,9 @@
 require "test_helper"
 
 class TasksControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @category = Category.new(name: "Food", description: "Test Food Category")
+  end
   #initial routes testing
   test "Testing for route:create" do
     assert_routing({ method: 'post', path: category_tasks_path(category_id: 1) }, { controller: 'tasks', action: 'create', category_id: "1" })
@@ -18,7 +21,47 @@ class TasksControllerTest < ActionDispatch::IntegrationTest
     assert_routing({ method: 'put', path: category_task_path(category_id: 1, id: 1) }, { controller: 'tasks', action: 'update', category_id: "1", id: "1" })
   end
 
+  #controller testing
 
-  
+  test "Action:Create should be able to create new task" do
+    #create category first
+    @category.save
+
+    assert_difference 'Task.count', 1 do
+      post category_tasks_path(@category.id), params: { task: { title: "Test Task", body: "Test Task Desc" } }
+    end
+
+    assert_response :success
+  end 
+
+  test "Action:Show should be able to show task" do
+    #create task
+    @category.tasks.new(title: "Task under Food", body: "Food Body").save
+    get category_task_path(category_id: @category.id, id: @category.tasks.first)
+    assert_response :success
+  end
+
+  test "Action:Update should be able to update task" do
+    #create task
+    @category.tasks.new(title: "Task under Food", body: "Food Body").save
+    new_task_title = "Updated Title"
+    new_task_body = "Updated Body"
+
+    put category_task_path(category_id: @category.id, id: @category.tasks.first), params: { task: { title: new_task_title, body: new_task_body } }
+
+    assert_equal new_task_title, @category.tasks.first.title
+    assert_equal new_task_body, @category.tasks.first.body
+    
+  end
+
+  test "Action:Delete should be able to update task" do
+    #create task
+    @category.tasks.new(title: "Task under Food", body: "Food Body").save
+
+    assert_difference 'Task.count', -1 do
+      delete category_task_path(category_id: @category.id, id: @category.tasks.first)
+      assert_response :success
+    end
+  end
 
 end
