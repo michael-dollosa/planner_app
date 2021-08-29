891368544f46d90d212ca1ea6e95b9ec3a02b864
Michael Dollosa <michaeldollosa@dolees-Macbook-Pro.local>
2021-08-30 05:29:32 +0800

    revamped a bit of design. removed edit and delete on cards. added show modals for tasks

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,7 +3,7 @@ class CategoriesController < ApplicationController
   def index
     @categories = Category.all
     @tasks = Task.all
-    @tasks_today = Task.all.where('due_date < ?', DateTime.now)
+    @tasks_today = Task.all.where('due_date <= ?', DateTime.now)
   end
 
   def new
@@ -18,13 +18,14 @@ class CategoriesController < ApplicationController
     @category = Category.new(category_params)
     @category.save
     #specify return redirect later
-    redirect_back(fallback_location: categories_path)
+    redirect_to(@category, fallback_location: categories_path)
   end
 
   def show
     @category = Category.find(params[:id])
     @categories = Category.all
     @tasks = @category.tasks
+    @tasks_today = Task.all.where('due_date <= ?', DateTime.now-1)
   end
 
   def edit
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -20,6 +20,10 @@ class TasksController < ApplicationController
 
   def show
     @task = Task.find(params[:id])
+    respond_to do |format|
+      format.html
+      format.js 
+    end
   end
   
   def edit
