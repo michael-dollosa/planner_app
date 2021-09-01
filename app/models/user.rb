7c9300dfabc10ba3119209f6ea670d14960fe419
Michael Dollosa <michaeldollosa@dolees-Macbook-Pro.local>
2021-09-02 05:26:37 +0800

    updated username validation to not be null

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          validates_uniqueness_of :username
+         validates_presence_of :username
 end
