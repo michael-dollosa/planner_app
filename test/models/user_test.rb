4cc4d122b3b358d7c71e27b37f41900c621d70ad
Michael Dollosa <michaeldollosa@dolees-Macbook-Pro.local>
2021-09-02 18:56:21 +0800

    added username test for devise user model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,18 +2,26 @@ require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
   def setup
-    @category = Category.new(name: "Food", description: "Test Food Category", user_id: users(:one).id)
     @user = User.new(email: "test@test.com", password: "test12345", password_confirmation: "test12345")
   end
 
   test "Should not be able to create user without email" do
+    @user = User.new(email: "test5@test.com", password: "test12345", password_confirmation: "test12345", username: "test user 5")
     @user.email = ""
     assert_not @user.save, "Saved without filling up required details"
   end
 
+  test "Should not be able to create user without username" do
+    @user = @user = User.new(email: "test5@test.com", password: "test12345", password_confirmation: "test12345")
+    @user.username = ""
+    assert_not @user.save, "Saved without filling up required details"
+  end
+
   test "Should not be able to create duplicate user" do
+    @user = User.new(email: "test4@test.com", password: "test12345", username: "test user 4", password_confirmation: "test12345")
     @user.save
-    user_duplicate = User.new(email: "test@test.com", password: "test12345", password_confirmation: "test12345")
+    user_duplicate = User.new(email: "test4@test.com", password: "test12345", username: "test user 4", password_confirmation: "test12345")
+    user_duplicate.save
     assert_not user_duplicate.save, "Created duplicate user"
   end
 
